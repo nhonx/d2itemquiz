a9f37eea9bd640b47c326e540a8c0ea386186ff2
nhonx <nhonx91@gmail.com>
2017-11-04 20:49:10 +0700

    patch 7.07

--- helper.js
+++ helper.js
@@ -4,9 +4,13 @@ function rnd(from, to) {
 
 function randomBuildItem(){
 	var i=rnd(60,itemcount);
-	while(item_in_element.indexOf(i)>=0 || history_item.indexOf(i)>=0){
+	if(test_items.length==0){
+		while(item_in_element.indexOf(i)>=0 || history_item.indexOf(i)>=0 || excluded_ids.indexOf(i)>=0){
 		i=rnd(60,itemcount);
-	}
+		}
+	} else {
+		i=rand.pickIn(test_items);
+	}	
 	history_item.push(i);
 	return i;
 }
